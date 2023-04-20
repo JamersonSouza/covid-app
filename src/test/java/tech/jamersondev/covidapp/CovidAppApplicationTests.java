963b489f30b2b1d9cb2ae233a5cda1b4f6246fed
jamersonsouza <jamersonsouza@outlook.com>
2023-04-20 08:40:24 -0300

    Commit inicial do projeto

---
+++ src/test/java/tech/jamersondev/covidapp/CovidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.covidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
