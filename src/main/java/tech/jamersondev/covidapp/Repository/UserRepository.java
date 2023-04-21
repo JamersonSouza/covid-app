46089738ddd1e20e2d9fccada702e4a51427b54b
jamersonsouza <jamersonsouza@outlook.com>
2023-04-21 15:14:29 -0300

    Método para buscar e-mail no banco de dados

--- src/main/java/tech/jamersondev/covidapp/Repository/UserRepository.java
+++ src/main/java/tech/jamersondev/covidapp/Repository/UserRepository.java
@@ -1,10 +1,13 @@
 package tech.jamersondev.covidapp.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tech.jamersondev.covidapp.Domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+    Optional<User> findByEmail(String email);
     
 }
