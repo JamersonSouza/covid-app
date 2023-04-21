8f1c69a006422696ff43e3e67f1814ae1ab45f2b
jamersonsouza <jamersonsouza@outlook.com>
2023-04-21 12:13:21 -0300

    Tratamento para errors do tipo Bad Request

--- src/main/java/tech/jamersondev/covidapp/Handlers/RestApiExceptionHandler.java
+++ src/main/java/tech/jamersondev/covidapp/Handlers/RestApiExceptionHandler.java
@@ -1,8 +1,27 @@
 package tech.jamersondev.covidapp.Handlers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import tech.jamersondev.covidapp.Exceptions.ErrorResponse;
+import tech.jamersondev.covidapp.Exceptions.ResourceBadRequestException;
+import tech.jamersondev.covidapp.Util.ConversorDate;
 
 @ControllerAdvice
 public class RestApiExceptionHandler {
     
+
+    @ExceptionHandler(ResourceBadRequestException.class)
+    public ResponseEntity<ErrorResponse> handlerResourceBadRequestException(ResourceBadRequestException excep){
+        
+        String dataHora = ConversorDate.converterDateParaDataHora(new Date());
+        ErrorResponse error = new ErrorResponse(dataHora, 
+        HttpStatus.BAD_REQUEST.value(), "Bad Request", excep.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
