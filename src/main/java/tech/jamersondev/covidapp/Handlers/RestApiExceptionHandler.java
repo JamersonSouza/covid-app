bcab7cfe688c4facaf535e296133ea19b40083c7
jamersonsouza <jamersonsouza@outlook.com>
2023-04-21 12:27:24 -0300

    Tratamento para Exception Not Found

--- src/main/java/tech/jamersondev/covidapp/Handlers/RestApiExceptionHandler.java
+++ src/main/java/tech/jamersondev/covidapp/Handlers/RestApiExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import tech.jamersondev.covidapp.Exceptions.ErrorResponse;
 import tech.jamersondev.covidapp.Exceptions.ResourceBadRequestException;
+import tech.jamersondev.covidapp.Exceptions.ResourceNotFoundException;
 import tech.jamersondev.covidapp.Util.ConversorDate;
 
 @ControllerAdvice
@@ -23,5 +24,15 @@ public class RestApiExceptionHandler {
         HttpStatus.BAD_REQUEST.value(), "Bad Request", excep.getMessage());
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlerResourceNotFoundException(ResourceNotFoundException excep){
+        String dataHora = ConversorDate.converterDateParaDataHora(new Date());
+
+        ErrorResponse error = new ErrorResponse(dataHora, HttpStatus.NOT_FOUND.value(), "Not Found Exception",
+         excep.getMessage());
+         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
