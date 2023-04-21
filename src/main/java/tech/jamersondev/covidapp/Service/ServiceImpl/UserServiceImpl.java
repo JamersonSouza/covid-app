c74a89fdf87a0c8f038591cdf5fb45433a2230a4
jamersonsouza <jamersonsouza@outlook.com>
2023-04-21 12:29:10 -0300

    Adição da anotação para o service, novo método de validação para cadastro do usuário e inclusão do Bcrypt

--- src/main/java/tech/jamersondev/covidapp/Service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/tech/jamersondev/covidapp/Service/ServiceImpl/UserServiceImpl.java
@@ -3,20 +3,26 @@ package tech.jamersondev.covidapp.Service.ServiceImpl;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import tech.jamersondev.covidapp.Domain.DTOs.User.UserRequestDTO;
 import tech.jamersondev.covidapp.Domain.DTOs.User.UserResponseDTO;
+import tech.jamersondev.covidapp.Exceptions.ResourceBadRequestException;
 import tech.jamersondev.covidapp.Repository.UserRepository;
 import tech.jamersondev.covidapp.Service.CrudService;
 
+@Service
 public class UserServiceImpl implements CrudService<UserRequestDTO, UserResponseDTO>{
 
     private ModelMapper mapper;
     private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(ModelMapper mapper, UserRepository userRepository) {
+    public UserServiceImpl(ModelMapper mapper, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.mapper = mapper;
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -47,6 +53,12 @@ public class UserServiceImpl implements CrudService<UserRequestDTO, UserResponse
     public void delete(Integer id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    public void validacaoCadastro(UserRequestDTO dto){
+        if(dto.getEmail().isEmpty() || dto.getSenha().isEmpty() || dto.getNome().isEmpty()){
+            throw new ResourceBadRequestException("Preencha todos os campos corretamente!");
+        }
     }
 
 
