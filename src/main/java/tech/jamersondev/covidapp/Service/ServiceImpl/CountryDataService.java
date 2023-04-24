ef085bd9f4f2092cca216ae57155b0306029a25d
jamersonsouza <jamersonsouza@outlook.com>
2023-04-24 15:53:06 -0300

    Método para buscar o resumo global e de todos os países

--- src/main/java/tech/jamersondev/covidapp/Service/ServiceImpl/CountryDataService.java
+++ src/main/java/tech/jamersondev/covidapp/Service/ServiceImpl/CountryDataService.java
@@ -3,17 +3,21 @@ package tech.jamersondev.covidapp.Service.ServiceImpl;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import tech.jamersondev.covidapp.Domain.DTOs.API.CountryDataDTO;
+import tech.jamersondev.covidapp.Domain.DTOs.API.CountrySummaryDataDTO;
+import tech.jamersondev.covidapp.Domain.DTOs.API.SummaryDataDTO;
 
 
 @Service
 public class CountryDataService {
 
-    private static final String REQUEST_PAISES = "https://api.covid19api.com/";
+    private static final String REQUESTS_API = "https://api.covid19api.com/";
 
     private RestTemplate restTemplate;
 
@@ -22,13 +26,16 @@ public class CountryDataService {
     }
 
     public List<CountryDataDTO> getPaises(){
-        String urlRequest = REQUEST_PAISES + "countries";
+        String urlRequest = REQUESTS_API + "countries";
         ResponseEntity<CountryDataDTO[]> paisesResponse = restTemplate.getForEntity(urlRequest, CountryDataDTO[].class);
         return Arrays.asList(paisesResponse.getBody());
         
     }
-    
 
-
-    
+    //método que busca um resumo do Mundo e dos Paises
+    public SummaryDataDTO getResumo(){
+        String urlRequest = REQUESTS_API + "summary";
+        SummaryDataDTO response = restTemplate.getForObject(urlRequest, SummaryDataDTO.class);
+        return response;
+    }
 }
