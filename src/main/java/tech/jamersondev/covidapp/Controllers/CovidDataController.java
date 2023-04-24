e08ab3fb894a8e210e1e71b30ca4ae4673b9c319
jamersonsouza <jamersonsouza@outlook.com>
2023-04-24 11:56:23 -0300

    Controller para Requisições da api com o método de buscar os paises implementado

---
+++ src/main/java/tech/jamersondev/covidapp/Controllers/CovidDataController.java
@@ -0,0 +1,28 @@
+package tech.jamersondev.covidapp.Controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tech.jamersondev.covidapp.Domain.DTOs.API.CountryDataDTO;
+import tech.jamersondev.covidapp.Service.ServiceImpl.CountryDataService;
+
+@RestController
+@RequestMapping("/api-covid")
+public class CovidDataController {
+
+    private CountryDataService countryDataService;
+
+    public CovidDataController(CountryDataService countryDataService) {
+        this.countryDataService = countryDataService;
+    }
+
+    
+    @GetMapping("/countries")
+    public List<CountryDataDTO> getCountries(){
+        return countryDataService.getPaises();
+    }
+    
+}
