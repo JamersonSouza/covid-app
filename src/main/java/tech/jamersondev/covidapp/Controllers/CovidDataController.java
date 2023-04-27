fc09a0c8066b1737f8b199dba55ca486a9e4f96e
Jamerson Souza <jamersonsouza@outlook.com>
2023-04-27 11:23:03 -0300

    remoção de dependencias não utilizadas

--- src/main/java/tech/jamersondev/covidapp/Controllers/CovidDataController.java
+++ src/main/java/tech/jamersondev/covidapp/Controllers/CovidDataController.java
@@ -1,8 +1,5 @@
 package tech.jamersondev.covidapp.Controllers;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
